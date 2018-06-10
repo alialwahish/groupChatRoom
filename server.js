fd03d3f25349ec775e89e41f34d7214e20d6aaca
Ali Bayati <bayati.ali@icloud.com>
2018-06-09 21:06:45 -0500

    added copyright

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path')
 const app=express();
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname+'/static'));
 
 app.set('views',path.join(__dirname,'public'));
 app.set('view engine','ejs');
